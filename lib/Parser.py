a300f9b741e685ad8186c3ad09d357b77f1ba3cf
F4llen4ngel <vanya_lebedev.2015@mail.ru>
2022-12-04 23:20:16 +0300

    working parser, group:user_list dataset

--- lib/Parser.py
+++ lib/Parser.py
@@ -1,6 +1,7 @@
 from fake_useragent import UserAgent
 import requests
 import json
+from time import sleep
 
 class Parser:
 
@@ -10,22 +11,44 @@ class Parser:
         self.user_agent = UserAgent().chrome
         self.headers = {'User-Agent': self.user_agent}
     
+    def request(self, url):
+        raw = requests.get(url, headers=self.headers)
+        try:
+            resp = json.loads(raw.text)["response"]
+        except:
+            resp = None
+            print(raw.text)
+        sleep(1)
+        return resp
+
     def getUserGroups(self, user_id):
         url = f"https://api.vk.com/method/users.getSubscriptions?&access_token={self.access_token}&user_id={user_id}&v=5.131"
-        raw = requests.get(url, headers=self.headers)
-        groupsList = json.loads(raw.text)
-        return groupsList["response"]["groups"]["items"]
+        resp = self.request(url)
+        if resp is None:
+            return []
+        return resp["groups"]["items"]
 
     def getGroupMembers(self, group_id):
         offset = 0
-        url = f"https://api.vk.com/method/groups.getMembers?&access_token={self.access_token}&group_id={group_id}&offset={offset}&v=5.131"
-        raw = requests.get(url, headers=self.headers) 
-        resp = json.loads(raw.text)["response"]
+        url = f"https://api.vk.com/method/groups.getMembers?&access_token={self.access_token}&group_id={group_id}&offset={offset}&v=5.131" 
+        resp = self.request(url)
+        if resp is None:
+            return []
         membersList = resp["items"]
         count = resp["count"]
         for offset in range(1000, count, 1000):
             url = f"https://api.vk.com/method/groups.getMembers?&access_token={self.access_token}&group_id={group_id}&offset={offset}&v=5.131"
-            raw = requests.get(url, headers=self.headers) 
-            resp = json.loads(raw.text)["response"]
+            resp = self.request(url)
             membersList += resp["items"]
         return membersList
+
+    def getGroupId(self, group_name):
+        url = f"https://api.vk.com/method/utils.resolveScreenName?&access_token={self.access_token}&screen_name={group_name}&v=5.131"
+        resp = self.request(url)
+        return resp["object_id"]
+
+    def getGroupName(self, group_id):
+        url = f"https://api.vk.com/method/groups.getById?&access_token={self.access_token}&group_id={group_id}&v=5.131"
+        resp = self.request(url)
+        return (resp["name"], resp["screen_name"])
+        
--- test.py
+++ test.py
@@ -1,6 +1,21 @@
+from time import sleep
+import json
+
 from lib.Parser import Parser
 
 parser = Parser()
-miem_members = list(set(parser.getGroupMembers(20122181)))
-print(len(miem_members))
-print(miem_members[-100:])
+
+with open("groups.txt") as f:
+    groups = list(set([x.strip().split('/')[-1] for x in f.readlines()]))
+
+gids = []
+for group in groups:
+    gids.append(parser.getGroupId(group))
+    sleep(1)
+
+a = dict()
+for gid in gids:
+    a[gid] = parser.getGroupMembers(gid)
+
+with open("users.txt", "w") as f:
+    f.write(json.dumps(a))
